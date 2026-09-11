0a303dd474d7e6921db17c25aefb1f25332e16bf
Tushar Pareek <pareektushar6@gmail.com>
2026-09-12 00:57:42 +0530

    Initialize App component with ComplaintForm and CopilotPanel

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import ComplaintForm from './components/ComplaintForm.jsx'
+import CopilotPanel from './components/CopilotPanel.jsx'
+
+export default function App() {
+  return (
+    <div className="app-layout">
+      <ComplaintForm />
+      <CopilotPanel />
+    </div>
+  )
+}
